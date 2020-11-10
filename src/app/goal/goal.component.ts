18aa8a57e4c04e5fa195aa035ccc8af05a664c12
Benardakaka <benardakaka484@gmail.com>
2020-11-10 22:31:05 +0300

    first commit

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+
+
+
+  export class GoalComponent implements OnInit {
+  
+    goToUrl(id){
+      this.router.navigate(['/goals',id])
+    }
+    goals:Goal[];
+    alertService:AlertService;
+    quote:Quote;
+    
+  addNewGoal(goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  deleteGoal(index){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
+    }
+  }
+
+  constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService, private http:HttpClient, private router:Router) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
+  }
+  ngOnInit() {
+
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+  }
+
+}
+
